228db76054a7fecb469f7a4590c8ca1e56486abf
CyberNovas <92268499+harrrshall@users.noreply.github.com>
2025-05-12 17:42:06 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  images: {
-    unoptimized: true
-  }
-}
+  // If you want to use Next.js default image optimization on Vercel:
+  // images: {
+  //   unoptimized: false, // Or simply remove the entire 'images' block
+  // },
 
-module.exports = nextConfig
+  // Or if you still want them unoptimized:
+  images: {
+    unoptimized: true,
+  }
+
+  // Add other configurations if you have them
+  // reactStrictMode: true,
+};
+
+module.exports = nextConfig;
